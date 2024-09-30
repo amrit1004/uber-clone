95d514a3d9298a843c9d4bd4d50fc56ad1569316
Amrit <amritsinghal2211@gmail.com>
2024-09-30 18:47:08 +0530

    map view direction

--- app/(root)/book-ride.tsx
+++ app/(root)/book-ride.tsx
@@ -1,93 +1,107 @@
 import { useUser } from "@clerk/clerk-expo";
+import { StripeProvider } from "@stripe/stripe-react-native";
 import { Image, Text, View } from "react-native";
 
+import Payment from "@/components/Payment";
 import RideLayout from "@/components/RideLayout";
 import { icons } from "@/constants";
 import { formatTime } from "@/lib/utils";
 import { useDriverStore, useLocationStore } from "@/store";
-import Payment from "@/components/Payment";
 
 const BookRide = () => {
   const { user } = useUser();
   const { userAddress, destinationAddress } = useLocationStore();
   const { drivers, selectedDriver } = useDriverStore();
- console.log(drivers)
+
   const driverDetails = drivers?.filter(
     (driver) => +driver.id === selectedDriver
   )[0];
 
   return (
-    <RideLayout title="Book Ride">
-      <>
-        <Text className="mb-3 text-xl font-JakartaSemiBold">
-          Ride Information
-        </Text>
+    <StripeProvider
+      publishableKey={process.env.EXPO_PUBLIC_STRIPE_PUBLISHABLE_KEY!}
+      merchantIdentifier="merchant.com.uber"
+      urlScheme="myapp"
+    >
+      <RideLayout title="Book Ride">
+        <>
+          <Text className="mb-3 text-xl font-JakartaSemiBold">
+            Ride Information
+          </Text>
 
-        <View className="flex flex-col items-center justify-center w-full mt-10">
-          <Image
-            source={{ uri: driverDetails?.profile_image_url }}
-            className="rounded-full w-28 h-28"
-          />
+          <View className="flex flex-col items-center justify-center w-full mt-10">
+            <Image
+              source={{ uri: driverDetails?.profile_image_url }}
+              className="rounded-full w-28 h-28"
+            />
 
-          <View className="flex flex-row items-center justify-center mt-5 space-x-2">
-            <Text className="text-lg font-JakartaSemiBold">
-              {driverDetails?.title}
-            </Text>
+            <View className="flex flex-row items-center justify-center mt-5 space-x-2">
+              <Text className="text-lg font-JakartaSemiBold">
+                {driverDetails?.title}
+              </Text>
 
-            <View className="flex flex-row items-center space-x-0.5">
-              <Image
-                source={icons.star}
-                className="w-5 h-5"
-                resizeMode="contain"
-              />
+              <View className="flex flex-row items-center space-x-0.5">
+                <Image
+                  source={icons.star}
+                  className="w-5 h-5"
+                  resizeMode="contain"
+                />
+                <Text className="text-lg font-JakartaRegular">
+                  {driverDetails?.rating}
+                </Text>
+              </View>
+            </View>
+          </View>
+
+          <View className="flex flex-col items-start justify-center w-full px-5 py-3 mt-5 rounded-3xl bg-general-600">
+            <View className="flex flex-row items-center justify-between w-full py-3 border-b border-white">
+              <Text className="text-lg font-JakartaRegular">Ride Price</Text>
+              <Text className="text-lg font-JakartaRegular text-[#0CC25F]">
+                ${driverDetails?.price}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-between w-full py-3 border-b border-white">
+              <Text className="text-lg font-JakartaRegular">Pickup Time</Text>
               <Text className="text-lg font-JakartaRegular">
-                {driverDetails?.rating}
+                {formatTime(parseInt(`${driverDetails?.time!}`))}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-between w-full py-3">
+              <Text className="text-lg font-JakartaRegular">Car Seats</Text>
+              <Text className="text-lg font-JakartaRegular">
+                {driverDetails?.car_seats}
               </Text>
             </View>
           </View>
-        </View>
 
-        <View className="flex flex-col items-start justify-center w-full px-5 py-3 mt-5 rounded-3xl bg-general-600">
-          <View className="flex flex-row items-center justify-between w-full py-3 border-b border-white">
-            <Text className="text-lg font-JakartaRegular">Ride Price</Text>
-            <Text className="text-lg font-JakartaRegular text-[#0CC25F]">
-              ${driverDetails?.price}
-            </Text>
+          <View className="flex flex-col items-start justify-center w-full mt-5">
+            <View className="flex flex-row items-center justify-start w-full py-3 mt-3 border-t border-b border-general-700">
+              <Image source={icons.to} className="w-6 h-6" />
+              <Text className="ml-2 text-lg font-JakartaRegular">
+                {userAddress}
+              </Text>
+            </View>
+
+            <View className="flex flex-row items-center justify-start w-full py-3 border-b border-general-700">
+              <Image source={icons.point} className="w-6 h-6" />
+              <Text className="ml-2 text-lg font-JakartaRegular">
+                {destinationAddress}
+              </Text>
+            </View>
           </View>
 
-          <View className="flex flex-row items-center justify-between w-full py-3 border-b border-white">
-            <Text className="text-lg font-JakartaRegular">Pickup Time</Text>
-            <Text className="text-lg font-JakartaRegular">
-              {formatTime(driverDetails?.time! || 5)}
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center justify-between w-full py-3">
-            <Text className="text-lg font-JakartaRegular">Car Seats</Text>
-            <Text className="text-lg font-JakartaRegular">
-              {driverDetails?.car_seats}
-            </Text>
-          </View>
-        </View>
-
-        <View className="flex flex-col items-start justify-center w-full mt-5">
-          <View className="flex flex-row items-center justify-start w-full py-3 mt-3 border-t border-b border-general-700">
-            <Image source={icons.to} className="w-6 h-6" />
-            <Text className="ml-2 text-lg font-JakartaRegular">
-              {userAddress}
-            </Text>
-          </View>
-
-          <View className="flex flex-row items-center justify-start w-full py-3 border-b border-general-700">
-            <Image source={icons.point} className="w-6 h-6" />
-            <Text className="ml-2 text-lg font-JakartaRegular">
-              {destinationAddress}
-            </Text>
-          </View>
-        </View>
-        <Payment/>
-      </>
-    </RideLayout>
+          <Payment
+            fullName={user?.fullName!}
+            email={user?.emailAddresses[0].emailAddress!}
+            amount={driverDetails?.price!}
+            driverId={driverDetails?.id}
+            rideTime={driverDetails?.time!}
+          />
+        </>
+      </RideLayout>
+    </StripeProvider>
   );
 };
 
--- components/Payment.tsx
+++ components/Payment.tsx
@@ -1,18 +1,97 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import CustomButton from './CustomButton'
+import { View, Text, Alert } from "react-native";
+import React from "react";
+import CustomButton from "./CustomButton";
+import { useState, useEffect } from "react";
+import { PaymentSheetError, useStripe } from "@stripe/stripe-react-native";
+import { Button } from "react-native";
+import { fetchAPI } from "@/lib/fetch";
+import { PaymentProps } from "@/types/type";
+const Payment = ({
+  fullName ,
+  email,
+  amount,
+  driverId,
+  rideTime,
+}: PaymentProps) => {
+  const { initPaymentSheet, presentPaymentSheet } = useStripe();
+  const [success, setSuccess] = useState(false);
+  const confirmHandler = async (
+    paymentMethod,
+    _,
+    intentCreationCallback
+  ) => {
+       const { paymentIntent, customer } = await fetchAPI(
+         "/(api)/(stripe)/create",
+         {
+           method: "POST",
+           headers: {
+             "Content-Type": "application/json",
+           },
+           body: JSON.stringify({
+             name: fullName || email.split("@")[0],
+             email: email,
+             amount: amount,
+             paymentMethodId: paymentMethod.id,
+           }),
+         }
+       );
+          if (paymentIntent.client_secret) {
+            const { result } = await fetchAPI("/(api)/(stripe)/pay", {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                payment_method_id: paymentMethod.id,
+                payment_intent_id: paymentIntent.id,
+                customer_id: customer,
+                client_secret: paymentIntent.client_secret,
+              }),
+            })
+          }
+    const { client_secret, error } = await response.json();
+    if (client_secret) {
+      intentCreationCallback({ clientSecret: client_secret });
+    } else {
+      intentCreationCallback({ error });
+    }
+  };
+  const initializePaymentSheet = async () => {
+    const { error } = await initPaymentSheet({
+      merchantDisplayName: "Example, Inc.",
+      intentConfiguration: {
+        mode: {
+          amount: 1099,
+          currencyCode: "USD",
+        },
+        confirmHandler: confirmHandler,
+      },
+    });
+    if (error) {
+      // handle error
+    }
+  };
+  const openPaymentSheet = async () => {
+    await initializePaymentSheet();
+    const { error } = await presentPaymentSheet();
 
-const Payment = () => {
-    const openPaymentSheet = async () =>{}
+    if (error) {
+      Alert.alert(`Error code: ${error.code}`, error.message);
+    } else {
+      setSuccess(true);
+    }
+    // Payment completed - show a confirmation screen.
+  };
   return (
     <>
-    <CustomButton
-     title='Confirm Ride'
-     className='my-10'
-     onPress={openPaymentSheet}
-    />
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
     </>
-  )
-}
+  );
+};
+export default Payment;
 
-export default Payment
+
